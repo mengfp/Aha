b1c31855bc9f548923e9c0fc85bd1c71b4396cd1
mengfp <mengfp@sina.com>
2025-03-06 13:49:56 -0500

    format

--- aha/aha.cpp
+++ aha/aha.cpp
@@ -17,95 +17,95 @@ std::string Version() {
   return VERSION;
 }
 
-#define AHA_IMPLEMENT(Model, Trainer, mix, trainer, V, T)                                 \
+#define AHA_IMPLEMENT(Model, Trainer, mix, trainer, V, T)                   \
                                                                             \
   Model::Model(int rank, int dim) {                                         \
-    p = new mix(rank, dim);                                              \
+    p = new mix(rank, dim);                                                 \
   }                                                                         \
                                                                             \
   Model::~Model() {                                                         \
-    delete (mix*)p;                                                      \
+    delete (mix*)p;                                                         \
   }                                                                         \
                                                                             \
   bool Model::Initialized() const {                                         \
-    return ((mix*)p)->Initialized();                                     \
+    return ((mix*)p)->Initialized();                                        \
   }                                                                         \
                                                                             \
   int Model::Rank() const {                                                 \
-    return ((mix*)p)->Rank();                                            \
+    return ((mix*)p)->Rank();                                               \
   }                                                                         \
                                                                             \
   int Model::Dim() const {                                                  \
-    return ((mix*)p)->Dim();                                             \
+    return ((mix*)p)->Dim();                                                \
   }                                                                         \
                                                                             \
   double Model::Predict(const V& x, V& y) const {                           \
-    return ((mix*)p)->Predict(x, y);                                     \
+    return ((mix*)p)->Predict(x, y);                                        \
   }                                                                         \
                                                                             \
   double Model::Predict(const std::vector<T>& x, std::vector<T>& y) const { \
     Map<const V> _x(x.data(), x.size());                                    \
     V _y;                                                                   \
-    auto r = ((mix*)p)->Predict(_x, _y);                                 \
+    auto r = ((mix*)p)->Predict(_x, _y);                                    \
     y.assign(_y.begin(), _y.end());                                         \
     return r;                                                               \
   }                                                                         \
                                                                             \
   void Model::Sort() {                                                      \
-    return ((mix*)p)->Sort();                                            \
+    return ((mix*)p)->Sort();                                               \
   }                                                                         \
                                                                             \
   std::string Model::Export() const {                                       \
-    return ((mix*)p)->Export();                                          \
+    return ((mix*)p)->Export();                                             \
   }                                                                         \
                                                                             \
   bool Model::Import(const std::string& model) {                            \
-    return ((mix*)p)->Import(model);                                     \
+    return ((mix*)p)->Import(model);                                        \
   }                                                                         \
                                                                             \
   Trainer::Trainer(Model& m) {                                              \
-    p = new trainer(*(mix*)*(void**)&m);                              \
+    p = new trainer(*(mix*)*(void**)&m);                                    \
   }                                                                         \
                                                                             \
   Trainer::~Trainer() {                                                     \
-    delete (trainer*)p;                                                  \
+    delete (trainer*)p;                                                     \
   }                                                                         \
                                                                             \
   int Trainer::Rank() const {                                               \
-    return ((trainer*)p)->Rank();                                        \
+    return ((trainer*)p)->Rank();                                           \
   }                                                                         \
                                                                             \
   int Trainer::Dim() const {                                                \
-    return ((trainer*)p)->Dim();                                         \
+    return ((trainer*)p)->Dim();                                            \
   }                                                                         \
                                                                             \
   void Trainer::Train(const V& sample) {                                    \
-    ((trainer*)p)->Train(sample);                                        \
+    ((trainer*)p)->Train(sample);                                           \
   }                                                                         \
                                                                             \
   void Trainer::Train(const std::vector<T>& sample) {                       \
     Map<const V> s(sample.data(), sample.size());                           \
-    ((trainer*)p)->Train(s);                                             \
+    ((trainer*)p)->Train(s);                                                \
   }                                                                         \
                                                                             \
   bool Trainer::Merge(const Trainer& t, double w) {                         \
-    return ((trainer*)p)->Merge(*(const trainer*)*(void**)&t, w);     \
+    return ((trainer*)p)->Merge(*(const trainer*)*(void**)&t, w);           \
   }                                                                         \
                                                                             \
   std::string Trainer::Spit() {                                             \
-    return ((trainer*)p)->Spit();                                        \
+    return ((trainer*)p)->Spit();                                           \
   }                                                                         \
                                                                             \
   bool Trainer::Swallow(const std::string& t, double w) {                   \
-    return ((trainer*)p)->Swallow(t, w);                                 \
+    return ((trainer*)p)->Swallow(t, w);                                    \
   }                                                                         \
                                                                             \
   double Trainer::Update(double noise_floor) {                              \
-    return ((trainer*)p)->Update(noise_floor);                           \
+    return ((trainer*)p)->Update(noise_floor);                              \
   }                                                                         \
                                                                             \
   void Trainer::Reset() {                                                   \
-    ((trainer*)p)->Reset();                                              \
+    ((trainer*)p)->Reset();                                                 \
   }
 
 AHA_IMPLEMENT(Model64, Trainer64, mix64, trainer64, VectorXd, double)
